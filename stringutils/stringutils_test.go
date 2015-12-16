ccb13ff1d54f1af8adf36b9aa4bf7709ba972abf
Rodney Rodriguez <rodneyxr@gmail.com>
2015-12-15 23:40:56 -0600

    Add three more Reverse functions to benchmark

--- stringutils/stringutils_test.go
+++ stringutils/stringutils_test.go
@@ -7,11 +7,12 @@ import (
 var forwardStrings = [...]string{`Hello`, `The quick bròwn 狐 jumped over the lazy 犬`, `1234567890`, `~!@#$%^&*()_+|\`, `ápplesareágoodfruit`}
 var backwardStrings = [...]string{`olleH`, `犬 yzal eht revo depmuj 狐 nwòrb kciuq ehT`, `0987654321`, `\|+_)(*&^%$#@!~`, `tiurfdoogáeraselppá`}
 
+// FIXME: Fails for combining characters (normalize input)
 func TestReverse(t *testing.T) {
 	// iterate through the forwardStrings array
 	for i, s := range forwardStrings {
 		// reverse the string
-		rev := Reverse(s)
+		rev := Reverse2(s)
 		// check if the reversed string matches its mirror array
 		if rev == backwardStrings[i] {
 			t.Logf("success: '%s'", s)
