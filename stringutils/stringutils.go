1341ef699d5dbeff3a98fa976b1e99febccbd276
Rodney Rodriguez <rodneyxr@gmail.com>
2015-12-16 16:17:27 -0600

    Performance improvements and benchmark for Reverse

--- stringutils/stringutils.go
+++ stringutils/stringutils.go
@@ -1,51 +1,13 @@
 package stringutils
 
-// TODO: Benchmark Reverse functions
-
 // Reverse - Reverses a string.
 func Reverse(s string) string {
 	c := []rune(s)
 	n := len(c)
-	for i := 0; i < n/2; i++ {
-		c[i], c[n-1-i] = c[n-1-i], c[i]
+	stop := n / 2
+	for i := 0; i < stop; i++ {
+		tmp := n - 1 - i
+		c[i], c[tmp] = c[tmp], c[i]
 	}
 	return string(c[n:])
-}
-
-// Reverse2 - Reverses a string.
-func Reverse2(s string) string {
-	n := len(s)
-	runes := make([]rune, n)
-	for _, rune := range s {
-		n--
-		runes[n] = rune
-	}
-	return string(runes[n:])
-}
-
-// Reverse3 - Reverses a string.
-func Reverse3(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-}
-
-// Reverse4 - Reverses a string.
-func Reverse4(s string) string {
-	// Get Unicode code points.
-	n := 0
-	rune := make([]rune, len(s))
-	for _, r := range s {
-		rune[n] = r
-		n++
-	}
-	rune = rune[0:n]
-	// Reverse
-	for i := 0; i < n/2; i++ {
-		rune[i], rune[n-1-i] = rune[n-1-i], rune[i]
-	}
-	// Convert back to UTF-8.
-	return string(rune)
 }
--- stringutils/stringutils_test.go
+++ stringutils/stringutils_test.go
@@ -12,12 +12,20 @@ func TestReverse(t *testing.T) {
 	// iterate through the forwardStrings array
 	for i, s := range forwardStrings {
 		// reverse the string
-		rev := Reverse2(s)
+		rev := Reverse(s)
 		// check if the reversed string matches its mirror array
 		if rev == backwardStrings[i] {
 			t.Logf("success: '%s'", s)
 		} else {
 			t.Errorf("fail: forward:'%s' != rev:'%s'", s, rev)
 		}
+	}
+}
+
+const benchmarkString string = "The quick brown fox jumped over the lazy dog."
+
+func BenchmarkReverse(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Reverse(benchmarkString)
 	}
 }
