3bae5b241a04522ec14785ea0e4f28b25f796eef
Rodney Rodriguez <rodneyxr@gmail.com>
2015-12-16 20:44:47 -0600

    Implement ReverseCC, test and benchmarks

--- stringutils/stringutils.go
+++ stringutils/stringutils.go
@@ -1,13 +1,50 @@
 package stringutils
 
-// Reverse - Reverses a string.
-func Reverse(s string) string {
-	c := []rune(s)
-	n := len(c)
-	stop := n / 2
-	for i := 0; i < stop; i++ {
-		tmp := n - 1 - i
-		c[i], c[tmp] = c[tmp], c[i]
+import (
+	"unicode"
+	"unicode/utf8"
+)
+
+// Reverse reverses a string but does not preserve combining characters. This function interprets its argument as UTF-8
+// and ignores bytes that do not form valid UTF-8. The return value is UTF-8.
+func Reverse(utf8Str string) string {
+	runes := make([]rune, len(utf8Str))
+	start := len(utf8Str)
+	for _, rune := range utf8Str {
+		// quietly skip invalid UTF-8
+		if rune != utf8.RuneError {
+			start--
+			runes[start] = rune
+		}
 	}
-	return string(c[n:])
+	return string(runes[start:])
+}
+
+// ReverseCC reverses a string while preserving combining characters. Interprets its argument as UTF-8 and ignores bytes
+// that do not form valid UTF-8. The return value is UTF-8. This version of function is about three times slower than Reverse.
+// You should only use this function if you require preservation of combining characters.
+func ReverseCC(utf8Str string) string {
+	if utf8Str == "" {
+		return ""
+	}
+	p := []rune(utf8Str)
+	r := make([]rune, len(p))
+	start := len(r)
+	for i := 0; i < len(p); {
+		// quietly skip invalid UTF-8
+		if p[i] == utf8.RuneError {
+			i++
+			continue
+		}
+		j := i + 1
+		for j < len(p) && (unicode.Is(unicode.Mn, p[j]) || unicode.Is(unicode.Me, p[j]) || unicode.Is(unicode.Mc, p[j])) {
+			j++
+		}
+		for k := j - 1; k >= i; k-- {
+			start--
+			r[start] = p[k]
+		}
+		i = j
+	}
+	return (string(r[start:]))
 }
--- stringutils/stringutils_test.go
+++ stringutils/stringutils_test.go
@@ -4,20 +4,32 @@ import (
 	"testing"
 )
 
-var forwardStrings = [...]string{`Hello`, `The quick bròwn 狐 jumped over the lazy 犬`, `1234567890`, `~!@#$%^&*()_+|\`, `ápplesareágoodfruit`}
-var backwardStrings = [...]string{`olleH`, `犬 yzal eht revo depmuj 狐 nwòrb kciuq ehT`, `0987654321`, `\|+_)(*&^%$#@!~`, `tiurfdoogáeraselppá`}
+var forwardStrings = [...]string{`Hello`, `1234567890`, `~!@#$%^&*()_+|\`, `ápplesareágoodfruit`, `asdf`}
+var reverseStrings = [...]string{`olleH`, `0987654321`, `\|+_)(*&^%$#@!~`, `tiurfdoogáeraselppá`, `fdsa`}
 
-// FIXME: Fails for combining characters (normalize input)
 func TestReverse(t *testing.T) {
 	// iterate through the forwardStrings array
 	for i, s := range forwardStrings {
 		// reverse the string
 		rev := Reverse(s)
 		// check if the reversed string matches its mirror array
-		if rev == backwardStrings[i] {
-			t.Logf("success: '%s'", s)
-		} else {
-			t.Errorf("fail: forward:'%s' != rev:'%s'", s, rev)
+		if rev != reverseStrings[i] {
+			t.Errorf("fail: forward:'%s' != reverse:'%s'", s, rev)
+		}
+	}
+}
+
+var forwardCCStrings = [...]string{`The quick bròwn 狐 jumped over the lazy 犬`, `ápplesareágoodfruit`, `as⃝df̅`}
+var reverseCCStrings = [...]string{`犬 yzal eht revo depmuj 狐 nwòrb kciuq ehT`, `tiurfdoogáeraselppá`, `f̅ds⃝a`}
+
+func TestReverseCC(t *testing.T) {
+	// iterate through the forwardStrings array
+	for i, s := range forwardCCStrings {
+		// reverse the string
+		rev := ReverseCC(s)
+		// check if the reversed string matches its mirror array
+		if rev != reverseCCStrings[i] {
+			t.Errorf("fail: forward:'%s' != reverse:'%s'", s, rev)
 		}
 	}
 }
@@ -27,5 +39,11 @@ const benchmarkString string = "The quick brown fox jumped over the lazy dog."
 func BenchmarkReverse(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Reverse(benchmarkString)
+	}
+}
+
+func BenchmarkReverseCC(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ReverseCC(benchmarkString)
 	}
 }
